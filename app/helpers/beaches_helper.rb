c027db50e61f1b70192fe8a97822548b6de2d114
Vladimir Dralo <vladimir@dralo.me>
2014-06-21 13:31:49 -0400

    Helpers methods to extract data from db added. Vlad killed Brandon. :(

--- app/helpers/beaches_helper.rb
+++ app/helpers/beaches_helper.rb
@@ -1,2 +1,73 @@
 module BeachesHelper
+
+  # Return an array of hashes with each beach data
+  # If beach passed to method, return 1 beach data, otherwise return all beaches data
+  # return 1 beach will look like this -> {:name, : lat, :long, : ecoli}
+  # if all beaches will look like this -> [{:name, :lat, :long, :ecoli}, {:name, :lat, :long, :ecoli}, ...]
+  def avg_3_or(beach=nil)
+    unless beach.nil? # return many beaches
+      @beaches = Beach.all
+      all_data = []
+      @beaches.all do |beach|
+        ecolis = beach.ecolis.order('date desc').select(:count).limit(3)
+        ecoli_num = ecolis.first > 100 ? ecolis.first : (ecolis.inject(0) {|t, e| t+=e, t})/3
+        beach_data = {name: beach.name, lat: beach.lat, long: beach.long, ecoli: ecoli_num}
+        all_data << beach_data
+      end
+      all_data
+    else # return  one beach
+      beach = Beach.find(beach) # ??? remove this line if  beach is instance object of Beach model
+      beach_data = {name: beach.name, lat: beach.lat, long: beach.long, ecoli: ecoli_num}
+    end
+  end
+
+  # Return integer of days - x days below 100 ecoli in a row
+  def ecoli_streak(beach)
+    days = 0
+    beach.ecolis.each do |ecoli|
+      if ecoli.count < 101
+        days += 1
+      else
+        return days
+      end
+    end
+  end
+
+  # Return data to build table for last 40 days or this summer, whichever has more days
+  # -> {:dates = [...], :ecoli = [...]}
+  def last_40_or_summer(beach)
+    ecolis = beach.ecolis.order('date asc').where('date > ?', Time.now.beginning_of_year)
+    if ecolis.count < 41
+      ecoli_data = {:dates = [], :ecoli = []}
+      ecolis.each do |ecoli|
+        ecoli_data[:dates] << ecoli.date
+        ecoli_data[:ecoli] << ecoli.count
+      end
+      ecoli_data
+    else
+      ecoli_data = summer_ecoli(beach, Time.now.year) # ? .year or .beginning_of_year
+    end
+  end
+
+  # Return data to build table for summer, devided by weeks
+  # -> {:dates = [...], :ecoli = [...]}
+  def summer_ecoli(beach, year)
+    start_period  = Time.new(year)
+    end_period    = Time.new(year+1)
+    ecolis = beach.ecolis.order('date asc').where('date > ? and date < ?', start_period, end_period)
+    ecoli_data = {:dates = [], :ecoli = []}
+
+    ecolis.each_slice(7) do |week_ecoli|
+      ecoli_data[:dates] << week_ecoli.first.date
+      ecoli_data[:ecoli] << (week_ecoli.inject(0) {|avg, e| avg += e; avg} / week_ecoli.count)
+    end
+
+    ecoli_data
+  end
+
 end
+
+
+
+
+
--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,3 +1,14 @@
 class Beach < ActiveRecord::Base
   has_many :ecolis
+
+  # ecoli data for last 7 days for certain beach
+  def last_7
+    ecolis.order('date desc').select(:count).limit(7)
+  end
+
+  # todays ecoli for certain beach
+  def today_ecoli(beach)
+    ecolis.order('date desc').select(:count).first
+  end
+
 end
