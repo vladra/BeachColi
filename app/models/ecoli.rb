6d8a685b9f009356ad983adbafd09dafdf3da3f0
brandoshmando <brancraft@gmail.com>
2014-06-21 17:02:41 -0400

    Changes to ecoli model

--- app/models/ecoli.rb
+++ app/models/ecoli.rb
@@ -9,11 +9,11 @@ class Ecoli < ActiveRecord::Base
         id = object["beachId"]
         date = Date.parse(object.css('sampleDate').text)
         count = object.css('eColiCount').text
+        count = Beach.find(id).ecolis.order('date desc').first.count if count.empty?
         advisory = object.css('beachAdvisory').text
         status = object.css('beachStatus').text
         Beach.find(id).ecolis << Ecoli.create(date: date, count: count, advisory: advisory, status: status)
       end
     end
   end
-
 end
